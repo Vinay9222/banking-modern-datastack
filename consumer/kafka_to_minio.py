f5108b555a2a5378981ff7df6f6bc0ffce710107
Vinay9222 <vinaychougale9222@gmail.com>
2026-08-14 17:46:11 +0530

    updated ruff files

--- consumer/kafka_to_minio.py
+++ consumer/kafka_to_minio.py
@@ -1,11 +1,11 @@
 import boto3
 from kafka import KafkaConsumer
 import json
-import pandas as pd
-from datetime import datetime
+# import pandas as pd
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import datetime, timezone
 
 # -----------------------------
 # Load secrets from .env
@@ -51,10 +51,13 @@ if bucket not in [b['Name'] for b in s3.list_buckets()['Buckets']]:
 def write_to_minio(table_name, records):
     if not records:
         return
-    df = pd.DataFrame(records)
-    date_str = datetime.now().strftime('%Y-%m-%d')
+    # df = pd.DataFrame(records)
+    # date_str = datetime.now().strftime('%Y-%m-%d')
+    date_str = datetime.now(timezone.utc).strftime('%Y-%m-%d')
     file_path = f'{table_name}_{date_str}.parquet'
-    df.to_parquet(file_path, engine='fastparquet', index=False)
+    # df.to_parquet(file_path, engine='fastparquet', index=False)
+    # Line 58
+    s3_key = f'{table_name}/date={date_str}/{table_name}_{datetime.now(timezone.utc).strftime("%H%M%S%f")}.parquet'
     s3_key = f'{table_name}/date={date_str}/{table_name}_{datetime.now().strftime("%H%M%S%f")}.parquet'
     s3.upload_file(file_path, bucket, s3_key)
     os.remove(file_path)

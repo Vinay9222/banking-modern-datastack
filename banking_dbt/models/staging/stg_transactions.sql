fa8e031291185d426f8b3115318c5790b9755645
Vinay9222 <vinaychougale9222@gmail.com>
2026-08-14 18:26:22 +0530

    fix(dbt): resolve duplicate rows in fact_transactions and stg_transactions

--- banking_dbt/models/staging/stg_transactions.sql
+++ banking_dbt/models/staging/stg_transactions.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
+{# {{ config(materialized='view') }}
 
 SELECT
     v:id::string                 AS transaction_id,
@@ -9,4 +9,41 @@ SELECT
     v:status::string             AS status,
     v:created_at::timestamp      AS transaction_time,
     CURRENT_TIMESTAMP            AS load_timestamp
-FROM {{ source('raw', 'transactions') }}
+FROM {{ source('raw', 'transactions') }} #}
+
+ {{ config(materialized='view') }}
+    
+    WITH parsed AS (
+        SELECT
+            v:id::string                 AS transaction_id,
+            v:account_id::string         AS account_id,
+            v:amount::float              AS amount,
+            v:txn_type::string           AS transaction_type,
+            v:related_account_id::string AS related_account_id,
+            v:status::string             AS status,
+            v:created_at::timestamp      AS transaction_time,
+            CURRENT_TIMESTAMP            AS load_timestamp
+        FROM {{ source('raw', 'transactions') }}
+    ),
+    
+    ranked AS (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY transaction_id
+                ORDER BY transaction_time DESC, load_timestamp DESC
+            ) AS rn
+        FROM parsed
+    )
+    
+    SELECT
+        transaction_id,
+        account_id,
+        amount,
+        transaction_type,
+        related_account_id,
+        status,
+        transaction_time,
+        load_timestamp
+    FROM ranked
+    WHERE rn = 1
